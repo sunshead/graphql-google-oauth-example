3aa18e999937970d00552f931254758576658096
Danjin SUN <sunshead@gmail.com>
2019-10-09 14:43:29 -0400

    fix config import

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { GoogleLogin } from 'react-google-login';
-import config from './config.json';
+import config from './config.js';
 
 class App extends Component {
   constructor() {
